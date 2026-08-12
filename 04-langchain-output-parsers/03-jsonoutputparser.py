f355c645752efb53d48190b95f01010bcb9b54e7
yash <yxshcodes@gmail.com>
2026-08-12 14:08:38 +0530

    langchain output parsers

--- 04-langchain-output-parsers/03-jsonoutputparser.py
+++ 04-langchain-output-parsers/03-jsonoutputparser.py
@@ -27,3 +27,6 @@ result = chain.invoke({'topic':'black hole'})
 
 print(result)
 
+
+# Output
+# {'facts_about_black_holes': [{'fact': 'A black hole is a region in space where the gravitational pull is so strong that nothing, not even light, can escapefrom it.'}, {'fact': 'Black holes can vary in size. They can be as small as a single atom or as large as billions of times the mass of the Sun.'}, {'fact':'The boundary surrounding a black hole is called the event horizon. Once something crosses this boundary, it cannot escape.'}, {'fact': 'Supermassive blackholes are believed to exist at the center of most galaxies, including our own Milky Way.'}, {'fact': "Black holes can distort time and space around them, aphenomenon predicted by Einstein's theory of General Relativity."}]}
